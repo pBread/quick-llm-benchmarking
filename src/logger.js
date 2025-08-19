26cf3cd6a5ae70eed643a0dd64e8d1765a48c8c5
pBread <4473570+pBread@users.noreply.github.com>
2025-08-19 15:54:26 -0600

    boiler

--- src/logger.js
+++ src/logger.js
@@ -2,12 +2,22 @@ import fs from "node:fs";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 import { createLogger, format, transports } from "winston";
+import kebabCase from "lodash/kebabCase";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const logsDir = path.resolve(__dirname, "..", "logs");
 
 fs.mkdirSync(logsDir, { recursive: true });
+
+export class BenchmarkLogger {
+  constructor() {}
+
+  start = new Date();
+  get id() {
+    return kebabCase(this.start.toISOString());
+  }
+}
 
 const makeFileTransport = (sid) =>
   new transports.File({
