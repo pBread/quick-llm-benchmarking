c42e57499f60c308793d7a0b3cee1a6bc3a5bbfb
pBread <4473570+pBread@users.noreply.github.com>
2025-08-19 21:27:58 -0600

    working through initial

---
+++ src/index.ts
@@ -0,0 +1,131 @@
+import "dotenv/config";
+import OpenAI from "openai";
+import type { ChatCompletionCreateParamsStreaming } from "openai/resources/index.mjs";
+import type { ResponseCreateParamsStreaming } from "openai/resources/responses/responses.mjs";
+
+const benchmarks: Benchmark[] = [
+  {
+    id: "openai-completions-gpt-4",
+    fn: composeOpenAICompletions({ model: "gpt-4" }),
+  },
+  {
+    id: "openai-completions-gpt-4-turbo",
+    fn: composeOpenAICompletions({ model: "gpt-4-turbo" }),
+  },
+  {
+    id: "openai-response-gpt-4",
+    fn: composeOpenAICompletions({ model: "gpt-4" }),
+  },
+  {
+    id: "openai-response-gpt-4-turbo",
+    fn: composeOpenAIResponse({ model: "gpt-4-turbo" }),
+  },
+];
+
+class Recorder {
+  beginAt?: Date;
+  endAt?: Date;
+  firstTokenAt?: Date;
+  tokens: TokenItem[] = [];
+
+  begin = () => {
+    this.beginAt = new Date();
+  };
+  end = () => {
+    this.endAt = new Date();
+  };
+
+  addToken = (token: string | undefined | null) => {
+    if (!token) return;
+
+    if (!this.firstTokenAt) this.firstTokenAt = new Date();
+    this.tokens.push({ content: token, createdAt: new Date() });
+  };
+}
+
+(async () => {
+  const prompt = "Tell me a joke";
+
+  const results = [];
+
+  for await (const bm of benchmarks) {
+    console.log("starting", bm.id);
+    const rec = new Recorder();
+    await bm.fn(rec, prompt);
+    results.push(rec);
+  }
+
+  console.log(results);
+})();
+
+// ========================================
+// Types
+// ========================================
+
+interface Benchmark {
+  id: string;
+  fn: Executor;
+}
+
+type Executor = (rec: Recorder, prompt: string) => Promise<void>;
+
+interface TokenItem {
+  content: string;
+  createdAt: Date;
+}
+
+// ========================================
+// Composers
+// ========================================
+function composeOpenAICompletions(
+  config: Omit<ChatCompletionCreateParamsStreaming, "messages" | "stream">,
+): Executor {
+  return async (rec, prompt) => {
+    const client = new OpenAI();
+
+    const stream = await client.chat.completions.create({
+      ...config,
+      stream: true,
+      messages: [{ role: "user", content: prompt }],
+    });
+
+    rec.begin();
+
+    for await (const chunk of stream) {
+      rec.addToken(chunk.choices[0]?.delta?.content);
+    }
+
+    rec.end();
+  };
+}
+
+function composeOpenAIResponse(
+  config: Omit<ResponseCreateParamsStreaming, "messages" | "stream">,
+): Executor {
+  return async (rec, prompt) => {
+    const client = new OpenAI();
+
+    rec.begin();
+    const stream = await client.responses.create({
+      ...config,
+      stream: true,
+      input: [{ role: "user", content: prompt }],
+    });
+
+    for await (const chunk of stream) {
+      if (
+        chunk.type === "response.output_item.added" &&
+        chunk.item.type === "message"
+      ) {
+        chunk.item.content
+          .filter((content) => content.type === "output_text") // filter refusals
+          .forEach((content) => rec.addToken(content.text));
+      }
+
+      if (chunk.type === "response.output_text.delta")
+        rec.addToken(chunk.delta);
+    }
+
+    rec.end();
+  };
+}
--- src/prompts.js
+++
@@ -1,7 +0,0 @@
-const prompts = [
-  `What is the capital of France?`,
-  `Who wrote Pride and Prejudice?`,
-  `Explain how to make an omelet`,
-];
-
-export default prompts;
