327111da7042566c78ceef787c7fb95c35881ae4
pBread <4473570+pBread@users.noreply.github.com>
2025-11-05 11:24:53 -0700

    added TypedEventEmitter utility

---
+++ src/typed-event-emitter.ts
@@ -0,0 +1,16 @@
+import { EventEmitter } from "node:events";
+
+/** EventEmitter with typecasting */
+export class TypedEventEmitter<Events = {}> extends EventEmitter {
+  emit = <K extends keyof Events & (string | symbol)>(
+    event: K,
+    ...args: Parameters<
+      Events[K] extends (...args: any[]) => any ? Events[K] : never
+    >
+  ): boolean => super.emit(event, ...args);
+
+  on = <K extends keyof Events & (string | symbol)>(
+    event: K,
+    listener: Events[K] extends (...args: any[]) => any ? Events[K] : never
+  ): this => super.on(event, listener);
+}
